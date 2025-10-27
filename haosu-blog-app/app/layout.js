d59f525a1ba88d17c89c4bcfde80ac22da15b2f3
TheKillerMockingbird <judah04olson@gmail.com>
2025-10-26 21:58:08 -0700

    Update1

--- haosu-blog-app/app/layout.js
+++ haosu-blog-app/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${Outfit.variable} antialiased`}
+        className={`${outfit.variable} antialiased`}
       >
         {children}
       </body>
